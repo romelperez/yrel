8df7279ad528b4991c61f9e3c412ce444a926995
Romel Perez <ronelprhone@gmail.com>
2023-09-14 13:35:23 -0500

    build: umd

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
+import path from 'path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
     include: ['e2e/**/*.test.ts']
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      formats: ['umd'],
+      name: 'yrel',
+      fileName: 'yrel',
+    },
+    rollupOptions: {
+      external: [],
+      output: {
+        dir: path.resolve(__dirname, 'build/umd')
+      }
+    }
   }
 })
